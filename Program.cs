af0755bf01196aa6dc8eec2a2e87645ff2a525e3
Tad Taylor <bittiez@gmail.com>
2023-12-20 21:59:57 -0700

    Add parser

--- Program.cs
+++ Program.cs
@@ -1,58 +1,79 @@
-﻿using System;
+﻿using CommandLine;
 
 namespace IsoTiloSlicer
 {
     internal class Program
     {
+        public class Options
+        {
+            [Option("image", Required = true)]
+            public string ImagePath { get; set; }
+
+            [Option("tilesize", Required = false)]
+            public int TileSize { get; set; } = 0;
+
+            [Option("offset", Required = false)]
+            public int Offset { get; set; } = 0;
+
+            [Option("filename", Required = false)]
+            public string FileName { get; set; }
+
+            [Option("output", Required = false)]
+            public string Output { get; set; }
+
+            [Option("startingnumber", Required = false)]
+            public int StartingNumber { get; set; } = 0;
+        }
+
         public static void Main(string[] args)
         {
             ImageHandler image = new ImageHandler();
 
-            if(args.Length == 0 )
+            if (args.Length == 0)
             {
-                Console.WriteLine("Usage: IsoTiloSlicer.exe -image path");
+                Console.WriteLine("Usage: IsoTiloSlicer.exe --image path");
                 Console.WriteLine("Optional args:");
-                Console.WriteLine("-tilesize 44  <- Tile size.");
-                Console.WriteLine("-offset 1  <- 0 if tile size is an odd number, 1 if it is an even number. Other numbers have unknown results.");
-                Console.WriteLine("-output out  <- Where to save sliced images to.");
-                Console.WriteLine("-filename {0} <- {0} is the image number, you can use tile{0} or file{0} for example.");
-                Console.WriteLine("-startingnumber 0 <- The file number to start with.");
+                Console.WriteLine("--tilesize 44  <- Tile size.");
+                Console.WriteLine("--offset 1  <- 0 if tile size is an odd number, 1 if it is an even number. Other numbers have unknown results.");
+                Console.WriteLine("--output out  <- Where to save sliced images to.");
+                Console.WriteLine("--filename {0} <- {0} is the image number, you can use tile{0} or file{0} for example.");
+                Console.WriteLine("--startingnumber 0 <- The file number to start with.");
+                return;
             }
 
-            foreach (string arg in args)
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => 
             {
-                switch(arg)
+                if (!string.IsNullOrEmpty(o.ImagePath))
                 {
-                    case "image":
-                        image.ImagePath = arg;
-                        break;
-                    case "tilesize":
-                        if (int.TryParse(arg, out int h))
-                        {
-                            image.TileHeight = h;
-                            image.TileWidth = h;
-                        }
-                        break;
-                    case "offset":
-                        if (int.TryParse(arg, out int o))
-                        {
-                            image.Offset = o;
-                        }
-                        break;
-                    case "output":
-                        image.OutputDirectory = arg;
-                        break;
-                    case "filename":
-                        image.FileNameFormat = arg;
-                        break;
-                    case "startingnumber":
-                        if (int.TryParse(arg, out int s))
-                        {
-                            image.StartingFileNumber = s;
-                        }
-                        break;
+                    image.ImagePath = o.ImagePath;
                 }
-            }
+
+                if(o.TileSize > 0)
+                {
+                    image.TileHeight = o.TileSize;
+                    image.TileWidth = o.TileSize;
+                }
+
+                if(o.Offset > 0)
+                {
+                    image.Offset = o.Offset;
+                }
+
+                if(!string.IsNullOrEmpty(o.Output))
+                {
+                    image.OutputDirectory = o.Output;
+                }
+
+                if (!string.IsNullOrEmpty(o.FileName))
+                {
+                    image.FileNameFormat = o.FileName;
+                }
+
+                if (o.StartingNumber != 0)
+                {
+                    image.StartingFileNumber = o.StartingNumber;
+                }
+            });
 
             image.Process();
         }
